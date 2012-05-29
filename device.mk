e6c5004c941e06bd41cb70add55a232af9ca01b2
Jonathan Bither <jonbither@gmail.com>
2012-05-29 00:58:43 -0400

    Add init.d script from nadlabak to try to prevent us from having to unplug and plug our device into usb

--- device.mk
+++ device.mk
@@ -169,6 +169,7 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PREBUILT)/etc/powervr.ini:system/etc/powervr.ini \
 	$(DEVICE_PREBUILT)/etc/vold.fstab:system/etc/vold.fstab \
 	$(DEVICE_PREBUILT)/etc/sysctl.conf:system/etc/sysctl.conf \
+	$(DEVICE_PREBUILT)/etc/init.d/00adb_during_boot:system/etc/init.d/00adb_during_boot \
 	$(DEVICE_PREBUILT)/etc/init.d/12scheduler:system/etc/init.d/12scheduler \
 	$(DEVICE_PREBUILT)/etc/init.d/13kernel:system/etc/init.d/13kernel \
 	$(DEVICE_PREBUILT)/etc/init.d/14multitouch:system/etc/init.d/14multitouch \
