895136757cc15e8b5437855406175f883a93c9d5
Jonathan Bither <jonbither@gmail.com>
2012-06-22 22:50:47 -0400

    Try to accelerate our conversions

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -2,13 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+FLTO_FLAG=$(call cc-option,"-flto", )
 LOCAL_MODULE_TAGS    := optional
 LOCAL_MODULE_PATH    := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE         := camera.omap3
 LOCAL_SRC_FILES      := cameraHal.cpp MotoCameraWrapper.cpp
 LOCAL_PRELINK_MODULE := false
-
-LOCAL_CFLAGS += -O3
+LOCAL_CFLAGS += -O3 -ffast-math $(FLTO_FLAG)
 
 LOCAL_C_INCLUDES += $(ANDROID_BUILD_TOP)/frameworks/base/include
 
